5e4e02b0a2c6fdc105fae98a8f3ae6bf36e0bf2b
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-10-27 18:35:51 +0500

    commit

---
+++ app.js
@@ -0,0 +1,66 @@
+// Chapter Objeccts
+
+// Question 1
+
+// let itemsArray = [
+//   { name: "juice", price: "50", quantity: "3" },
+//   { name: "cookie", price: "30", quantity: "9" },
+//   { name: "shirt", price: "880", quantity: "1" },
+//   { name: "pen", price: "100", quantity: "2" }
+// ]
+
+// let totalPriceAll = 0
+
+// for(let i =0; i<itemsArray.length; i++){
+//   let itemTotal = Number(itemsArray[i].price) * Number(itemsArray[i].quantity)
+//   console.log(`Total price of ${itemsArray[i].name}: ${itemTotal}`)
+
+//   totalPriceAll += itemTotal
+// }
+
+// console.log(`Total price of all items: ${totalPriceAll}`)
+
+// Question 2
+
+// Create the object
+// let user = {
+//   name: "Ali Khan",
+//   email: "ali@example.com",
+//   password: "12345",
+//   age: 25,
+//   gender: "Male",
+//   city: "Karachi",
+//   country: "Pakistan"
+// }
+
+
+// console.log("age" in user)     
+// console.log("country" in user)   
+
+// console.log("firstName" in user) 
+// console.log("lastName" in user)  
+
+// Question 3
+
+// function data(a, b, c, d, e) {
+
+//     this.name = a
+//     this.age = b
+//     this.gender = c
+//     this.course = d
+//     this.city = e
+
+// }
+
+// let student1 = new data("Ali", 22, "Male", "Computer Science", "Karachi");
+// let student2 = new data("Sara", 20, "Female", "Mathematics", "Lahore");
+// let student3 = new data("John", 23, "Male", "Physics", "Islamabad");
+
+// console.log(student1)
+// console.log(student2)
+// console.log(student3)
+
+// Question 4
+
+
+
